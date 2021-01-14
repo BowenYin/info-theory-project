1e1dd23e4180ec62dc349006177320c28a7e7487
Bowen Yin <bowenvictoryin@gmail.com>
2021-01-14 22:09:31 +0000

    Battleship: code some comment

--- battleship/battleship_v02.cpp
+++ battleship/battleship_v02.cpp
@@ -25,14 +25,14 @@ int main() {
         for (int j = 0; j <= 10-ships[ship]; j++) {
           int hits = 0;
           bool ok = true;
-          for (int j2 = j; j2 < j+ships[ship]; j2++) {
+          for (int j2 = j; j2 < j+ships[ship]; j2++) { // check if a ship fits
             if (board[i][j2] != 0 && board[i][j2] != 2) {
               ok = false;
               break;
             } if (board[i][j2] == 2) hits++;
           }
           if (!ok) continue;
-          for (int j2 = j; j2 < j+ships[ship]; j2++) {
+          for (int j2 = j; j2 < j+ships[ship]; j2++) { // adjust weights for ship coordinates
             probs[i][j2] += 1+hits*100;
           }
         }
@@ -41,14 +41,14 @@ int main() {
         for (int j = 0; j < 10; j++) {
           int hits = 0;
           bool ok = true;
-          for (int i2 = i; i2 < i+ships[ship]; i2++) {
+          for (int i2 = i; i2 < i+ships[ship]; i2++) { // check if a ship fits
             if (board[i2][j] != 0 && board[i2][j] != 2) {
               ok = false;
               break;
             } if (board[i2][j] == 2) hits++;
           }
           if (!ok) continue;
-          for (int i2 = i; i2 < i+ships[ship]; i2++) {
+          for (int i2 = i; i2 < i+ships[ship]; i2++) { // adjust weights for ship coordinates
             probs[i2][j] += 1+hits*100;
           }
         }
@@ -56,7 +56,7 @@ int main() {
     }
     int max_prob = 0;
     int max_i, max_j;
-    for (int j = 0; j < 10; j++) {
+    for (int j = 0; j < 10; j++) { // find coordinates with max probability/weight
       for (int i = 0; i < 10; i++) {
         if (board[i][j] == 0 && probs[i][j] > max_prob) {
           max_prob = probs[i][j];
@@ -71,35 +71,35 @@ int main() {
     cout << "> ";
     cin >> input;
     transform(input.begin(), input.end(), input.begin(), ::tolower);
-    if (input.substr(0, 1) == "m") {
+    if (input.substr(0, 1) == "m") { // miss
       board[max_i][max_j] = 1;
-    } else if (input.substr(0, 1) == "h")
+    } else if (input.substr(0, 1) == "h") // hit
       board[max_i][max_j] = 2;
-    else if (input.substr(0, 1) == "s") {
+    else if (input.substr(0, 1) == "s") { // sink
       if (ships.size() <= 1) break;
       board[max_i][max_j] = 3;
       int ship_len = 1;
-      for (int i = max_i-1; i >= 0 && i >= max_i-4; i--)
+      for (int i = max_i-1; i >= 0 && i >= max_i-4; i--) // search upwards (in the array) for previous hits
         if (board[i][max_j] == 2) {
           board[i][max_j] = 3;
           ship_len++;
         } else break;
-      for (int i = max_i+1; i < 10 && i <= max_i+4; i++)
+      for (int i = max_i+1; i < 10 && i <= max_i+4; i++) // search downwards
         if (board[i][max_j] == 2) {
           board[i][max_j] = 3;
           ship_len++;
         } else break;
-      for (int j = max_j-1; j >= 0 && j >= max_j-4; j--)
+      for (int j = max_j-1; j >= 0 && j >= max_j-4; j--) // search leftwards
         if (board[max_i][j] == 2) {
           board[max_i][j] = 3;
           ship_len++;
         } else break;
-      for (int j = max_j+1; j < 10 && j <= max_j+4; j++)
+      for (int j = max_j+1; j < 10 && j <= max_j+4; j++) // search rightwards
         if (board[max_i][j] == 2) {
           board[max_i][j] = 3;
           ship_len++;
         } else break;
-      auto it = find(ships.begin(), ships.end(), ship_len);
+      auto it = find(ships.begin(), ships.end(), ship_len); // remove sunken ship from list
       ships.erase(it);
     }
     if (ships.size() == 0) break;
